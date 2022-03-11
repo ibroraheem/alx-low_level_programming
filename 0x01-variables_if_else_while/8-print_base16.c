1f3b0e851b11c0078198d59b6c613eb78f659908
Ibrahim Abdulraheem <ibroraheem95@gmail.com>
2022-03-11 11:46:09 -0800

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,17 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints numbers 0 to 9.
+ * main - Prints the alphabet.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	char num[16] = "0123456789abcdef";
 	int i;
 
 	for (i = 0; i < 16; i++)
 	{
-		putchar(i);
+		putchar(num[i]);
 	}
 	putchar('\n');
 	return (0);
